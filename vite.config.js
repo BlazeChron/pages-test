4db45f98b583ebad8b686cbf18b91f89d6bd34e9
BlazeChron <blazechron@JasonsAwesomeComputer.>
2025-12-26 16:12:51 +0800

    Update base url vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // change base url
+  base: '/pages-test/',
   // moving source code to src folder, and changing output directory
   root: 'src',
   build: {
